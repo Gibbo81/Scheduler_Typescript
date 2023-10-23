054dbf82380b693859daf4c3bdf244da3a782646
Masi Giuliano <masigiuliano@yahoo.it>
2023-10-23 20:59:12 +0200

    Add unit tests to deletefile Action

--- src/businessLogic/Actions/FileAction.ts
+++ src/businessLogic/Actions/FileAction.ts
@@ -14,17 +14,25 @@ export class FileAction implements Action{ //TODO: maybe is the base class not n
 
 export class DeleteFileAction implements Action{
 
-    constructor(private folder:string, private subName:string){}
+    constructor(protected folder:string, private subName:string){}
    
     async execute(): Promise<{ [key: string]: string; }> {
-        var files = (await fs.readdir(this.folder)) //TODO:: unit test with two protected method?
-        files = this.ApplyFilter(files);
+        var files = await this.readAllfiles() 
+        files = this.applyFilter(files);
         for (var f of files.map(fileName => this.folder + fileName))
-            await fs.unlink(f)
+            await this.deleteFile(f)
         return {'DeleteFileAction' : 'Completed'}
     }
 
-    private ApplyFilter(files: string[]) {
+    protected async deleteFile(f: string): Promise<undefined> {
+        await fs.unlink(f);
+    }
+
+    protected async readAllfiles(): Promise<string[]> {
+        return await fs.readdir(this.folder);
+    }
+
+    private applyFilter(files: string[]) {
         if (this.subName)
             files = files.filter(f => f.includes(this.subName));
         return files;
---
+++ src/test/businessLogicTests/Actions/DeleteFileAction.test.ts
@@ -0,0 +1,71 @@
+import { DeleteFileAction } from "../../../businessLogic/Actions/FileAction"
+
+test('No files are present therefore no files are deleted', async () =>{        
+    var action = new DeleteFileActionWrapper("", "", [])
+    
+    var result = await action.execute()
+
+    expect(action.Deleted.length).toBe(0)
+    expect(result.DeleteFileAction).toBe('Completed')
+  }
+)
+
+test('Four files are present without any filter (empty string), all are deleted', async () =>{        
+    var noFilterEmptyString =''
+    var action = new DeleteFileActionWrapper("", noFilterEmptyString, ["aa1", "1aa2", "gth", "uaua2"])
+    
+    var result = await action.execute()
+
+    expect(action.Deleted.length).toBe(4)
+    expect(result.DeleteFileAction).toBe('Completed')
+  }
+)
+
+test('Four files are present without any filter (null), all are deleted', async () =>{        
+    var noFilter_Null =null
+    var action = new DeleteFileActionWrapper("", noFilter_Null, ["aa1", "1aa2", "gth", "uaua2"])
+    
+    var result = await action.execute()
+
+    expect(action.Deleted.length).toBe(4)
+    expect(result.DeleteFileAction).toBe('Completed')
+  }
+)
+
+test('Four files are present without any filter (undefined), all are deleted', async () =>{        
+    var noFilter_Undefined =undefined
+    var action = new DeleteFileActionWrapper("", noFilter_Undefined, ["aa1", "1aa2", "gth", "uaua2"])
+    
+    var result = await action.execute()
+
+    expect(action.Deleted.length).toBe(4)
+    expect(result.DeleteFileAction).toBe('Completed')
+  }
+)
+
+test('Four files are present but only two matche the filter, two are deleted', async () =>{        
+    var filter ='aa'
+    var action = new DeleteFileActionWrapper("", filter, ["1aa2", "gth", "aa1", "uaua2"])
+    
+    var result = await action.execute()
+
+    expect(action.Deleted.length).toBe(2)
+    expect(action.Deleted[0]).toBe('1aa2')
+    expect(action.Deleted[1]).toBe('aa1')
+    expect(result.DeleteFileAction).toBe('Completed')
+  }
+)
+
+class DeleteFileActionWrapper extends DeleteFileAction{
+    public Deleted : string[] = []
+    constructor(folder:string,  subName:string, private filesPresent :string[]){ super(folder, subName) }
+
+    protected override async readAllfiles(): Promise<string[]> {
+        return Promise.resolve(this.filesPresent)
+    }
+    
+    protected override async deleteFile(f: string): Promise<undefined> {
+        this.Deleted.push(f)
+        return Promise.resolve(undefined)
+    }
+}
