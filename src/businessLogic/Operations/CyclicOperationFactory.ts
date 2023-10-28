7254d5d6b15c04f8f88b35ce19e6d174f2eb1ef9
Masi Giuliano <masigiuliano@yahoo.it>
2023-10-28 14:48:33 +0200

    Add action creation to CyclicOperationFactory

--- src/businessLogic/Operations/CyclicOperationFactory.ts
+++ src/businessLogic/Operations/CyclicOperationFactory.ts
@@ -1,18 +1,17 @@
 import { CheckCyclicOperation } from "../../SqlLite/CheckCyclicOperation";
 import { CyclingConfiguratrion } from "../../service/dto/CyclingConfiguratrion";
+import { ActionFactory } from "../Actions/ActionFactory";
 import { CyclicOperation } from "./CyclicOperation";
 
 
-
-
 export class CyclicOperationFactory {
-    constructor(private db: string, private schedureId: number) { }
+    constructor(private dbConnectionString: string, private schedureId: number, private actionFactory: ActionFactory) { }
 
     build(configuration: CyclingConfiguratrion): CyclicOperation {
         return new CyclicOperation(configuration.CyclingTime, 
                                    configuration.OperationName,
                                    this.schedureId,
-                                   new CheckCyclicOperation(this.db, this.schedureId), 
-                                   []); //TODO: missing actions factory
+                                   new CheckCyclicOperation(this.dbConnectionString, this.schedureId), 
+                                   configuration.Actions.map(a => this.actionFactory.create(a))); 
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,14 @@ import { ReadConfiguration } from "./service/ReadConfigurations"
 import { CheckCyclicOperation } from "./SqlLite/CheckCyclicOperation"
 import { CyclicOperationFactory } from "./businessLogic/Operations/CyclicOperationFactory"
 import { DeleteFileAction } from "./businessLogic/Actions/DeleteFileAction"
+import { ActionFactory } from "./businessLogic/Actions/ActionFactory"
 
 var sqllite='C:/Repo/Scheduler_Typescript/src/SqlLite/Test_DB/Scheduler.db'
 
-//ReadConfigurationToDTO()
+ReadConfigurationTest()
 //StartPluto()
 //var y = readStatusPluto()
-DeleteFileActionTestsManual(undefined)
+//DeleteFileActionTestsManual(undefined)
 
 console.log(100)
 
@@ -33,10 +34,10 @@ function  DeleteFileActionTestsManual(filter: string ){
     a.execute().then(x => console.log(`result : ${JSON.stringify(x)}`))
 }
 
-function ReadConfigurationToDTO() {
+function ReadConfigurationTest() {
     var reader = new ReadConfiguration('C:/Repo/Scheduler_Typescript/ActionsConfiguration/Fixed/',
                                        'C:/Repo/Scheduler_Typescript/ActionsConfiguration/Cyclic/',
-                                       new CyclicOperationFactory(sqllite, 34))
+                                       new CyclicOperationFactory(sqllite, 34, new ActionFactory()))
     reader.load()
         .then(x => {
             console.log(`data : ${JSON.stringify(x)}`)
